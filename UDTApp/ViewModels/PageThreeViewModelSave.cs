62e8baeef2f6a6c683f1ecb0fbdb4c1a1c898b15
gibc <gib.cardwell@gmail.com>
2017-07-15 20:28:05 -0600

    add untracked files

---
+++ UDTApp/ViewModels/PageThreeViewModelSave.cs
@@ -0,0 +1,192 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using UDTApp.Models;
+
+namespace UDTApp.ViewModels
+{
+
+    public class PageThreeViewModelSave : PageOneViewModel
+    {
+        public DelegateCommand SaveChangesCommand { get; set; }
+
+        public PageThreeViewModelSave()
+        {
+            //IsMasterVisible = false;
+            //IsDetailVisible = true;
+            //IsRelationVisible = true;
+            //IsItemVisible = false;
+
+            SaveChangesCommand = new DelegateCommand(saveChanges);
+
+
+        }
+
+        //override public bool IsInputEnabled
+        //{
+            //get { return (ChildSelectedItem != null || _newDataSet != null); }
+        //}
+
+        ////private ObservableCollection<DataSetRelation> _dataRelaionItems;
+        ////public ObservableCollection<DataSetRelation> DataRelationItems
+        //////public dynamic DataItems
+        ////{
+        ////    get { return _dataRelaionItems; }
+        ////    set
+        ////    {
+        ////        SetProperty(ref _dataRelaionItems, value);
+        ////        AddCommand.RaiseCanExecuteChanged();
+        ////        DeleteCommand.RaiseCanExecuteChanged();
+        ////    }
+        ////}
+
+        ////override public int SelectedIndex
+        ////{
+        ////    get { return base.SelectedIndex; }
+        ////    set 
+        ////    { 
+        ////        base.SelectedIndex = value;
+        ////        ChildOptions = null;
+        ////    }
+        ////}
+
+        private int _childSelectedIndex = -1;
+        public int ChildSelectedIndex
+        {
+            get { return _childSelectedIndex; }
+            set
+            {
+                SetProperty(ref _childSelectedIndex, value);
+                //RaisePropertyChanged("ChildSelectedItem");
+            }
+        }
+
+        public ObservableCollection<String> getChildOptions(string parent, string child)
+        {
+            string childDataSet = child; // value.ChildDateSet;
+            //List<string> excludeDataSets = DataSetList.Sets[SelectedIndex].DataSetRelations
+            List<string> excludeDataSets = DataSetList.Sets[77].DataSetRelations
+                .Where(y => y.ChildDateSet != childDataSet).
+                Select(x => x.ChildDateSet).ToList();
+            excludeDataSets.Add(parent);
+
+            List<string> allDataSets = DataSetList.Sets.Select(x => x.Name).ToList();
+            List<string> result = allDataSets.Except(excludeDataSets).ToList();
+            if (child.Length == 0)
+                result.Add(child);
+
+            return new ObservableCollection<String>(result);
+        }
+
+        //private DataSetRelation _childSelectedItem;
+        //public DataSetRelation ChildSelectedItem
+        //{
+        //    get
+        //    { return _childSelectedItem; }
+        //    set
+        //    {
+        //        SetProperty(ref _childSelectedItem, value);
+        //        if (value != null)
+        //        {
+        //            var parentDataSet = DataSetList.Sets[SelectedIndex].Name;
+
+        //            ChildOptions = getChildOptions(parentDataSet, value.ChildDateSet);
+        //            SelectedChild = value.ChildDateSet;
+        //        }
+        //        else
+        //        {
+        //            SelectedChild = "";
+        //        }
+        //        DeleteCommand.RaiseCanExecuteChanged();
+        //        CancelCommand.RaiseCanExecuteChanged();
+        //        SaveCommand.RaiseCanExecuteChanged();
+        //        RaisePropertyChanged("IsInputEnabled");
+        //        _newDataSet = null;
+        //    }
+        //}
+
+        private int _comboIndex = -1;
+        public int ComboIndex
+        {
+            get { return _comboIndex; }
+            set
+            {
+                SetProperty(ref _comboIndex, value);
+                //SaveCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private string _parentName;
+        [Required]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 15 characters.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Name can include only letter characters")]
+        public string ParentDataSet
+        {
+            get
+            {
+                //if (SelectedIndex > -1)
+                    //_parentName = DataSets[SelectedIndex].Name;
+                return _parentName;
+            }
+            //set
+            //{
+            //    SetProperty(ref _parentName, value);
+            //    SaveCommand.RaiseCanExecuteChanged();
+            //    CancelCommand.RaiseCanExecuteChanged();
+            //}
+        }
+
+        private string _selectedChild = null;
+        [CustomValidation(typeof(PageThreeViewModel), "CheckChildDataSet")]
+        public string SelectedChild
+        {
+            get { return _selectedChild; }
+            set
+            {
+                SetProperty(ref _selectedChild, value);
+                //SaveCommand.RaiseCanExecuteChanged();
+                //CancelCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+
+        private ObservableCollection<String> _childOptions = null;
+        public ObservableCollection<String> ChildOptions
+        {
+            get
+            {
+                return _childOptions;
+            }
+            set
+            {
+                SetProperty(ref _childOptions, value);
+            }
+        }
+
+        // coment for commit
+        private void saveChanges()
+        {
+            DataSetList.SaveChanges();
+        }
+
+
+        public static System.ComponentModel.DataAnnotations.ValidationResult CheckChildDataSet(string childDataSet, ValidationContext context)
+        {
+            if (childDataSet != null && childDataSet.Length == 0)
+                return new System.ComponentModel.DataAnnotations.ValidationResult("Please select a child dataset from the drop-down list.");
+            else return System.ComponentModel.DataAnnotations.ValidationResult.Success;
+
+        }
+
+    }
+}
---
+++ UDTApp/ViewModels/PageTwoViewModelSave.cs
@@ -0,0 +1,119 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using UDTApp.Models;
+
+namespace UDTApp.ViewModels
+{
+    public class PageTwoViewModelSave : ViewModelBase<DataItem, DataItem>
+    {
+        public PageTwoViewModelSave()
+        {
+            IsMasterVisible = false;
+            IsDetailVisible = true;
+            IsRelationVisible = false;
+            IsItemVisible = true;
+        }
+
+        override public bool IsPropertyEdited
+        {
+            get
+            {
+                if (ChildSelectedIndex == -1) return false;
+                return DataItems[ChildSelectedIndex].Name != ChildName ||
+                    DataItems[ChildSelectedIndex].Type.ToString() != Type;
+            }
+        }
+
+        override public void SetTextProps(DataItem dataSet, string value = "")
+        {
+            ChildName = value;
+            Type = value;
+            if (dataSet != null)
+            {
+                ChildName = dataSet.Name;
+                Type = dataSet.Type.ToString();
+            }
+        }
+
+        override public void SetMasterTextProps(DataSet dataSet, string value = "") { }
+
+        override public void SetChildTextProps(DataItem dataSet, string value = "") 
+        {
+            SetTextProps(dataSet, value);
+        }
+
+ 
+        override public void LoadTextProps(DataItem dataSet)
+        {
+            dataSet.Name = ChildName;
+            dataSet.Type = Convert.ToInt32(Type);
+        }
+        override public DataItem CreateNewDataSet()
+        {
+            // returns C
+            return new DataItem();
+        }
+        override public ObservableCollection<DataItem> GetSelectedCol(int selectedIndex)
+        {
+            // takes nothing, returns D
+            //return EditedCol[selectedIndex].DataItems;
+            return DataSets[selectedIndex].DataItems;
+        }
+        override public ObservableCollection<DataItem> EditedCol
+        {
+            // returns C
+            get { return DataItems; }
+        }
+
+        override public int EditedIndex { get { return ChildSelectedIndex; } set { ChildSelectedIndex = value; } }
+
+        override public bool IsInputEnabled
+        {
+            get { return (ChildSelectedItem != null || _newDataSet != null); }
+        }
+
+
+        private string _name;
+        [Required]
+        [StringLength(15, MinimumLength = 4, ErrorMessage = "Name must be between 4 and 15 characters.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Name can include only letter characters")]
+        public string ChildName
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                SetProperty(ref _name, value);
+                SaveCommand.RaiseCanExecuteChanged();
+                CancelCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private string _type;
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                SetProperty(ref _type, value);
+                SaveCommand.RaiseCanExecuteChanged();
+                CancelCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+    }
+}
