76aa14bfa724b6f7ba217290a15d180b6b07d0a2
gibc <gib.cardwell@gmail.com>
2017-12-11 13:05:17 -0700

    Initial commit

---
+++ Metric/Metric/Controllers/FileController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Metric.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        [HttpPost]
+        public ActionResult Index()
+        {
+            //return View();
+            {
+                foreach (string upload in Request.Files)
+                {
+                    if (Request.Files[upload].FileName != "")
+                    {
+                        string path = AppDomain.CurrentDomain.BaseDirectory + "/App_Data/uploads/";
+                        string filename = Path.GetFileName(Request.Files[upload].FileName);
+                        Request.Files[upload].SaveAs(Path.Combine(path, filename));
+                    }
+                }
+                return View("Upload");
+            }
+        }
+
+        // GET: File/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: File/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: File/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: File/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: File/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: File/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: File/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Metric/Metric/Controllers/TestController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace Metric.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpPost]
+        public ActionResult Index()
+        {
+            //return View();
+            {
+                foreach (string upload in Request.Files)
+                {
+                    if (Request.Files[upload].FileName != "")
+                    {
+                        string path = AppDomain.CurrentDomain.BaseDirectory + "App_Data/";
+                        string filename = Path.GetFileName(Request.Files[upload].FileName);
+                        Request.Files[upload].SaveAs(Path.Combine(path, filename));
+                    }
+                }
+                return View("Upload");
+            }
+        }
+
+        public ActionResult Downloads()
+        {
+            var dir = new System.IO.DirectoryInfo(Server.MapPath("~/App_Data"));
+            System.IO.FileInfo[] fileNames = dir.GetFiles("*.*"); List<string> items = new List<string>();
+            foreach (var file in fileNames)
+            {
+                items.Add(file.Name);
+            }
+            return View(items);
+        }
+
+        public FileResult Download(string ImageName)
+        {
+            var FileVirtualPath = "~/App_Data/" + ImageName;
+            return File(FileVirtualPath, "application/force-download", Path.GetFileName(FileVirtualPath));
+        }
+    }
+}
