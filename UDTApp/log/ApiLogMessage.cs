6603da543352f528c00d219d4ca37275c769a188
gibc <gib.cardwell@gmail.com>
2017-12-20 14:44:49 -0700

    add untracted files from udtapp

---
+++ UDTApp/log/ApiLogMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDTApp.Log
+{
+    // dup code with same class in Metric web app project
+    public class ApiLogMessage
+    {
+        public string AppHost { get; set; }
+        public string DateTime { get; set; }
+        public string UserName { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ UDTApp/log/Log.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace UDTApp.Log
+{
+    public class Log
+    {
+        public static void LogMessage(string msg)
+        {
+            //string url = "http://localhost:54946/api/log/post";
+            string url = "http://metricresearch.org/api/log/post";
+            using (WebClient client = new WebClient())
+            {
+                ApiLogMessage apiMsg = new ApiLogMessage();
+                apiMsg.AppHost = Environment.MachineName;
+                apiMsg.UserName = Environment.UserName;
+                apiMsg.DateTime = DateTime.Now.ToString();
+                apiMsg.Message = msg;
+
+                // Set the header so it knows we are sending JSON.
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                // Serialise the data we are sending in to JSON
+                JavaScriptSerializer jss = new JavaScriptSerializer();
+                string serialisedData = jss.Serialize(apiMsg);
+                // Make the request
+                string response = client.UploadString(url, serialisedData);
+                // Deserialise the response
+                string result = (string)jss.DeserializeObject(response);
+            }
+
+        }
+    }
+}
