bb81893d8168433063a30a3bfb33add96325f243
gibc <gib.cardwell@gmail.com>
2017-12-20 13:35:45 -0700

    commit without config

--- Metric/Metric/DAL/LogMessageContext.cs
+++ Metric/Metric/DAL/LogMessageContext.cs
@@ -8,7 +8,7 @@ using System.Web;
 
 namespace Metric.DAL
 {
-    // comment for commit text
+    // another comment for commit text
     public class LogMessageContext : DbContext
     {
         public LogMessageContext() : base("LoggingConnection")
