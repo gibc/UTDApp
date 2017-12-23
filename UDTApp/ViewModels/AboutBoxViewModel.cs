12c959d789e98b35f72846891f95c97c5c4c3d11
gibc <gib.cardwell@gmail.com>
2017-12-23 11:56:24 -0700

    App image code mods

--- UDTApp/ViewModels/AboutBoxViewModel.cs
+++ UDTApp/ViewModels/AboutBoxViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using Prism.Commands;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Configuration;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace UDTApp.ViewModels
 {
@@ -21,7 +24,9 @@ namespace UDTApp.ViewModels
             Assembly assembly = Assembly.GetExecutingAssembly();
             version = assembly.GetName().Version;
 
+            versionInfo = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location);
         }
+        FileVersionInfo versionInfo = null;
         private Version version = null;
         private Window aboutBoxView { get; set; }
         private Action closeAction { get; set; }
@@ -29,6 +34,14 @@ namespace UDTApp.ViewModels
         {
             closeAction = new Action(window.Close);
             aboutBoxView = window;
+            window.ResizeMode = ResizeMode.NoResize;
+            Hyperlink webLink = (Hyperlink)window.FindName("webLink");
+            webLink.RequestNavigate += WebLink_RequestNavigate;
+        }
+
+        private void WebLink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.ToString());
         }
 
         public string minorRevision
@@ -44,6 +57,46 @@ namespace UDTApp.ViewModels
             get
             {
                 return version.Build.ToString();
+            }
+        }
+
+        public string companyName
+        {
+            get
+            {
+                return versionInfo.CompanyName;
+            }
+        }
+
+        public string copyright
+        {
+            get
+            {
+                return versionInfo.LegalCopyright;
+            }
+        }
+
+        public string description
+        {
+            get
+            {
+                return versionInfo.FileDescription;
+            }
+        }
+
+        public string title
+        {
+            get
+            {
+                return versionInfo.ProductName;
+            }
+        }
+
+        public string titlecopyright
+        {
+            get
+            {
+                return title + " " + copyright;
             }
         }
 
