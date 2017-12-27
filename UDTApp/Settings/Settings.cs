72c3241f192bb3c856815aa4bac1e6dbb92c9269
gibc <gib.cardwell@gmail.com>
2017-12-26 19:51:55 -0700

    add files

---
+++ UDTApp/Settings/Settings.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace UDTApp.Settings
+{
+    public class AppSettings
+    {
+        public AppSettings()
+        {
+            fileSettings = new List<FileSetting>();
+        }
+
+        static private AppSettings _appSettings = null;
+        static public AppSettings appSettings
+        {
+            get
+            {
+                if(_appSettings == null)
+                {
+                    _appSettings = Read();
+                }
+                return _appSettings;
+            }
+        }
+
+        public List<FileSetting> fileSettings
+        {
+            get;
+            set;
+        }
+
+        public void addFile(string _filePath)
+        {
+            FileSetting setting = fileSettings.Find(fs => fs.filePath == _filePath);
+            if(setting != null)
+            {
+                fileSettings.Remove(setting);
+                setting.dateTime = DateTime.Now.ToString();
+            }
+            else
+            {
+                setting = new FileSetting() { dateTime = DateTime.Now.ToString(), filePath = _filePath };
+            }
+
+            autoOpenFile = setting;
+            fileSettings.Add(setting);
+            fileSettings.OrderByDescending(fs => fs.dateTime);
+            if(fileSettings.Count > 10)
+            {
+                fileSettings.RemoveRange(10, fileSettings.Count - 10);
+            }
+            
+        }
+
+        private FileSetting _autoOpenFile = null;
+        public FileSetting autoOpenFile
+        {
+            get { return _autoOpenFile; }
+            set { _autoOpenFile = value; }
+        }
+
+        private bool _designView = false;
+        public bool designView
+        {
+            get { return _designView; }
+            set { _designView = value; }
+        }
+
+        static public void Save(AppSettings appSettings, string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                XmlSerializer xmls = new XmlSerializer(typeof(AppSettings));
+                xmls.Serialize(sw, appSettings);
+            }
+        }
+
+        static private AppSettings Read()
+        {
+            using (StreamReader sw = new StreamReader(settingFilePath))
+            {
+                XmlSerializer xmls = new XmlSerializer(typeof(AppSettings));
+                return xmls.Deserialize(sw) as AppSettings;
+            }
+        }
+
+        public static string settingFilePath
+        {
+            get
+            {
+                string tempFolder = Path.GetTempPath();
+                tempFolder += "udtSettings";
+                if (!Directory.Exists(tempFolder))
+                    Directory.CreateDirectory(tempFolder);
+                string settingsFile = tempFolder + "\\settings.xml";
+                if(!File.Exists(settingsFile))
+                {
+                    Save(new AppSettings(), settingsFile);
+                }
+                return settingsFile;
+            }
+        }
+    }
+}
