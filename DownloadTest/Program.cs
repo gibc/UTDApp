4f301b175583a78cc22f27b4abbfcc1ea89e9e34
gibc <gib.cardwell@gmail.com>
2018-02-07 12:15:55 -0700

    add files

--- DownloadTest/Program.cs
+++ DownloadTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using s3.amazon.com.docsamples;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Configuration;
 using System.IO;
 using System.Linq;
@@ -8,14 +9,152 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 namespace DownloadTest
 {
+    public class A : IXmlSerializable
+    {
+        public A()
+        {
+            tableData = new ObservableCollection<B>();
+        }
+
+        //public string personName
+        //{
+        //    get;
+        //    set;
+        //}
+
+        private ObservableCollection<B> _tableData;
+        public ObservableCollection<B> tableData
+        {
+            get
+            {
+                return _tableData;
+            }
+            set
+            {
+                _tableData = value;
+            }
+        }
+
+        // Xml Serialization Infrastructure
+
+        public void WriteXml(XmlWriter writer)
+        {
+            //writer.WriteAttributeString("personName", tableData);
+            writer.WriteStartElement("tableData");
+            writer.WriteAttributeString("count", tableData.Count.ToString());
+            var otherSer = new XmlSerializer(typeof(B));
+            foreach (B other in tableData)
+            {
+                otherSer.Serialize(writer, other);
+            }
+            writer.WriteEndElement();
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            //personName = reader.GetAttribute("personName");
+            //string xml = reader.ReadInnerXml();
+            int count;
+            reader.MoveToContent();
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name == "tableData")
+                    {
+                        XElement el = XNode.ReadFrom(reader) as XElement;
+                        if(el.FirstAttribute.Name == "count")
+                        {
+                            count = Int32.Parse(el.FirstAttribute.Value);
+                        }
+                        foreach(XElement node in el.Nodes())
+                        {
+                            if(node.NodeType == XmlNodeType.Element)
+                            {
+                                var otherSer = new XmlSerializer(typeof(B));
+                                if (node.Name == "B")
+                                {
+                                    B other = (B)otherSer.Deserialize(new StringReader(node.ToString()));
+                                    //tableData.Add(new B() { b_name = node.Value });
+                                    tableData.Add(other);
+                                }
+                            }
+                        }
+                    }
+
+                }
+                //if(reader.NodeType == XmlNodeType.Attribute)
+                //{
+
+                //}
+            }
+
+            //reader.ReadStartElement("A");
+            //xml = reader.ReadInnerXml();
+
+            //reader.ReadStartElement("tableData");
+
+            //xml = reader.ReadInnerXml();
+            //if (reader.MoveToAttribute("count"))
+            //if (reader.MoveToElement())
+            //{
+            //    var v = reader.GetAttribute("count");
+            //}
+            ////var v = reader.Value;
+            //int count = int.Parse(reader.Value);
+
+            //var otherSer = new XmlSerializer(typeof(B));
+            //for (int i = 0; i < count; i++)
+            //{
+            //    var other = (B)otherSer.Deserialize(reader);
+            //    tableData.Add(other);
+            //}
+
+            //reader.ReadEndElement();
+            //reader.ReadEndElement();
+        }
+
+        public XmlSchema GetSchema()
+        {
+            return (null);
+        }
+    }
+
+    public class B
+    {
+        public B() { }
+        public string b_name
+        {
+            get;
+            set;
+        }
+    }
+
     //https://s3-us-west-2.amazonaws.com/udtdownloads/SqlLocalDB.msi
     class Program
     {
         private static long GetURLFileLength(string url, string fileName)
         {
+            //A a = new A();
+            //a.tableData.Add(new B() { b_name = "first" });
+            //a.tableData.Add(new B() { b_name = "second" });
+
+            //XmlSerializer serializer = new XmlSerializer(a.GetType());
+
+            //using (StringWriter writer = new StringWriter())
+            //{
+            //    serializer.Serialize(writer, a);
+
+            //    string XML = writer.ToString();
+            //}
+
             long length = -1;
             var webRequest = HttpWebRequest.Create(
                 string.Format("https://s3-us-west-2.amazonaws.com/udtdownloads/{0}", fileName));
@@ -29,24 +168,24 @@ namespace DownloadTest
             return length;
         }
 
-            //url += string.Format("?fileName={0}", fileName);
-            //var webReq = (HttpWebRequest)WebRequest.Create(url);
-            //webReq.Method = "GET";
-            //try
-            //{
-            //    WebResponse response = webReq.GetResponse();
-            //    long length = 0;
+        //url += string.Format("?fileName={0}", fileName);
+        //var webReq = (HttpWebRequest)WebRequest.Create(url);
+        //webReq.Method = "GET";
+        //try
+        //{
+        //    WebResponse response = webReq.GetResponse();
+        //    long length = 0;
 
-            //    using (Stream stream = response.GetResponseStream())
-            //    {
-            //        using (StreamReader sr = new StreamReader(stream))
-            //        {
-            //            length = Int64.Parse(sr.ReadToEnd());
-            //        }
-            //    }
-            //    Console.WriteLine("File {0} length = {1}", fileName, length);
+        //    using (Stream stream = response.GetResponseStream())
+        //    {
+        //        using (StreamReader sr = new StreamReader(stream))
+        //        {
+        //            length = Int64.Parse(sr.ReadToEnd());
+        //        }
+        //    }
+        //    Console.WriteLine("File {0} length = {1}", fileName, length);
 
-            //}
+        //}
         //    catch (Exception ex)
         //    {
         //        Console.WriteLine("For file '{0}' {1}", fileName, ex.Message);
@@ -114,6 +253,28 @@ namespace DownloadTest
         static string url = "http://localhost:54946/api/Download/FileLength";
         static void Main(string[] args)
         {
+            A a = new A();
+            //a.personName = "my name";
+            a.tableData.Add(new B() { b_name = "first" });
+            a.tableData.Add(new B() { b_name = "second" });
+
+            XmlSerializer serializer = new XmlSerializer(a.GetType());
+
+            string XML = "";
+            using (StringWriter writer = new StringWriter())
+            {
+                serializer.Serialize(writer, a);
+
+                XML = writer.ToString();
+            }
+
+
+            using (TextReader reader = new StringReader(XML))
+            {
+                A result = serializer.Deserialize(reader) as A;
+            }
+
+
             //UploadObject.AWStest(null);
             //return;
 
@@ -124,9 +285,9 @@ namespace DownloadTest
             //GetURLFileLength(url, "missing");
 
             //url = "http://localhost:54946/api/Download/GetFile";
-            Task<bool> t = GetURLContentsAsync(url, "SqlLocalDB.msi");
-            t.Wait();
-            if (!t.Result) Console.WriteLine("file load failed");
+            //Task<bool> t = GetURLContentsAsync(url, "SqlLocalDB.msi");
+            //t.Wait();
+            //if (!t.Result) Console.WriteLine("file load failed");
 
             //t = GetURLContentsAsync(url, "UtdAppPublish.zip");
             //t.Wait();
---
+++ UDTApp/ListManager/EventList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UDTApp.Models;
+
+namespace UDTApp.ListManager
+{
+    public class ManagedObservableCollection<T> : ObservableCollection<UDTData>
+    {
+        public void Add(T item)
+        {
+            // if not present, add to dictionary
+            // else get from dictionary
+            UDTData dataItem = item as UDTData;
+            if(dataItem != null)
+            {
+                if (itemDic.ContainsKey(dataItem.objId))
+                {
+                    List<UDTData> dataList = null;
+                    if (itemDic.TryGetValue(dataItem.objId, out dataList))
+                    {
+                        if (!dataList.Contains(dataItem))
+                            dataList.Add(dataItem);
+                        dataItem = dataList[0];
+                    }
+                }
+                else
+                {
+                    List<UDTData> dataList = new List<UDTData>();
+                    dataList.Add(dataItem);
+                    itemDic.Add(dataItem.objId, dataList);
+                }
+            }
+            if (deletedTableList.Contains(dataItem.Name))
+                deletedTableList.Remove(dataItem.Name);
+            base.Add(dataItem);
+        }
+
+        public void Remove(T item)
+        {
+            // remove item only when last reference to the item is removed
+            UDTData dataItem = item as UDTData;
+            if (dataItem != null)
+            {
+                if (itemDic.ContainsKey(dataItem.objId))
+                {
+                    List<UDTData> dataList = null;
+                    if (itemDic.TryGetValue(dataItem.objId, out dataList))
+                    {
+                        if(dataList.Count > 1)
+                        {
+                            dataList.Remove(dataList[Count - 1]);
+                            return;
+                        }
+                        else
+                        {
+                            dataList.Clear();
+                            deletedTableList.Add(dataItem.Name);
+                        }
+                    }
+                }
+            }
+            base.Remove(dataItem);
+        }
+
+        private static Dictionary<Guid, List<UDTData>> itemDic = new Dictionary<Guid, List<UDTData>>();
+
+        public static List<String> deletedTableList = new List<string>();
+    
+    }
+}
